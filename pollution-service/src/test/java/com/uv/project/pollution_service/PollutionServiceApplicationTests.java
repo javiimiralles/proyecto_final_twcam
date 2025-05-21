aa7052b945d2e8d8706c9edca537a6f9e8cbc656
jmm291 <jmm291@gcloud.ua.es>
2025-05-21 16:56:51 +0200

    initial commit

---
+++ pollution-service/src/test/java/com/uv/project/pollution_service/PollutionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uv.project.pollution_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollutionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
