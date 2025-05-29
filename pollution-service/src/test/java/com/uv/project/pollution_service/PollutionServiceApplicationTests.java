dd6d2a2c3fdd4f8e38f861a54346b94a688a6f74
celiakimera <ciguort@teleco.upv.es>
2025-05-29 13:46:24 +0200

    pruebas

--- pollution-service/src/test/java/com/uv/project/pollution_service/PollutionServiceApplicationTests.java
+++ pollution-service/src/test/java/com/uv/project/pollution_service/PollutionServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.uv.project.pollution_service;
+// package com.uv.project.pollution_service;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class PollutionServiceApplicationTests {
+// @SpringBootTest
+// class PollutionServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
