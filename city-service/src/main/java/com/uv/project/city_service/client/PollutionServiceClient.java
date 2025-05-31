23dc984aaa9905e18c06c121f8fa5431593d027b
celiakimera <ciguort@teleco.upv.es>
2025-05-31 18:48:50 +0200

    token pollucion

--- city-service/src/main/java/com/uv/project/city_service/client/PollutionServiceClient.java
+++ city-service/src/main/java/com/uv/project/city_service/client/PollutionServiceClient.java
@@ -1,5 +1,47 @@
 package com.uv.project.city_service.client;
 
+import com.uv.project.city_service.providers.RestTemplateProvider;
+import com.uv.project.shared.domain.Estacion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class PollutionServiceClient {
-    
+
+    @Autowired
+    private RestTemplateProvider restTemplateProvider;
+
+    @Value("${pollution-service.url}")
+    private String baseUrl;
+
+    @Value("${keycloak.admin-token}")
+    private String adminToken;
+
+    public Estacion createEstacion(Estacion estacion) {
+        RestTemplate restTemplate = restTemplateProvider.withToken(adminToken);
+        ResponseEntity<Estacion> response = restTemplate.postForEntity(
+            baseUrl + "/api/v1/estacion",
+            estacion,
+            Estacion.class
+        );
+        return response.getBody();
+    }
+
+    public Estacion updateEstacion(int id, Estacion estacion) {
+        RestTemplate restTemplate = restTemplateProvider.withToken(adminToken);
+        estacion.setId(id); 
+        restTemplate.put(
+            baseUrl + "/api/v1/estacion/" + id,
+            estacion
+        );
+        return estacion;
+    }
+
+    public void deleteEstacion(int id) {
+        RestTemplate restTemplate = restTemplateProvider.withToken(adminToken);
+        restTemplate.delete(baseUrl + "/api/v1/estacion/" + id);
+    }
 }
