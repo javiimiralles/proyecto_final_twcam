4d3fcc6e3aedc2d79db6c9ca3b4657e0ed10a4bd
celiakimera <ciguort@teleco.upv.es>
2025-06-02 00:02:04 +0200

    endpoint estacion ayuntamiento

---
+++ city-service/src/main/java/com/uv/project/city_service/controllers/EstacionController.java
@@ -0,0 +1,40 @@
+package com.uv.project.city_service.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import com.uv.project.shared.domain.Estacion;
+import com.uv.project.city_service.services.EstacionService;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EstacionController {
+
+    @Autowired
+    private EstacionService estacionService;
+
+    @PreAuthorize("hasRole('admin')")
+    @PostMapping("/estacion")
+    public ResponseEntity<Estacion> createEstacion(@RequestBody Estacion estacion) {
+        try {
+            Estacion created = estacionService.createEstacion(estacion);
+            return ResponseEntity.ok(created);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PreAuthorize("hasRole('admin')")
+    @DeleteMapping("/estacion/{id}")
+    public ResponseEntity<Void> deleteEstacion(@PathVariable Integer id) {
+        try {
+            estacionService.deleteEstacion(id);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
---
+++ city-service/src/main/java/com/uv/project/city_service/repository/EstacionRepository.java
@@ -0,0 +1,7 @@
+package com.uv.project.city_service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.uv.project.shared.domain.Estacion;
+
+public interface EstacionRepository extends JpaRepository<Estacion, Integer> {
+}
