1f5da4b1af7787568e0594b20c399a78424fb2c0
jmm291 <jmm291@gcloud.ua.es>
2025-06-10 19:41:22 +0200

    avances en deployment

--- docker_build.sh
+++ docker_build.sh
@@ -5,31 +5,31 @@
 echo "Building Docker images for microservices..."
 
 cd bike-data
-docker build -t javiimiralles/bike-data:v1.0 .
+docker build -t javiimiralles/bike-data:v1.1 .
 cd ..
 
 cd bike-service
-docker build -t javiimiralles/bike-service:v1.3 .
+docker build -t javiimiralles/bike-service:v1.4 .
 cd ..
 
 cd city-service
-docker build -t javiimiralles/city-service:v1.2 .
+docker build -t javiimiralles/city-service:v1.3 .
 cd ..
 
 cd config-server
-docker build -t javiimiralles/config-server:v1.3 .
+docker build -t javiimiralles/config-server:v1.4 .
 cd ..
 
 cd pollution-data
-docker build -t javiimiralles/pollution-data:v1.0 .
+docker build -t javiimiralles/pollution-data:v1.1 .
 cd ..
 
 cd pollution-service
-docker build -t javiimiralles/pollution-service:v1.2 .
+docker build -t javiimiralles/pollution-service:v1.3 .
 cd ..
 
 cd schedulers
-docker build -t javiimiralles/schedulers:v1.2 .
+docker build -t javiimiralles/schedulers:v1.3 .
 cd ..
 
 echo "Docker images built successfully."
@@ -40,12 +40,12 @@ docker login
 echo "Pushing Docker images to Docker Hub..."
 
 # Subimos las imágenes a Docker Hub
-docker push javiimiralles/bike-data:v1.0
-docker push javiimiralles/bike-service:v1.3
-docker push javiimiralles/city-service:v1.2
-docker push javiimiralles/config-server:v1.3
-docker push javiimiralles/pollution-data:v1.0
-docker push javiimiralles/pollution-service:v1.2
-docker push javiimiralles/schedulers:v1.2
+docker push javiimiralles/bike-data:v1.1
+docker push javiimiralles/bike-service:v1.4
+docker push javiimiralles/city-service:v1.3
+docker push javiimiralles/config-server:v1.4
+docker push javiimiralles/pollution-data:v1.1
+docker push javiimiralles/pollution-service:v1.3
+docker push javiimiralles/schedulers:v1.3
 
 echo "Docker images pushed to Docker Hub successfully."
